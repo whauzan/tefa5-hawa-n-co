37dd74a3a0a9c2a536c0f9cf714b6b56a5c6f825
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-10-17 17:50:43 +0700

    fix: styling about page

--- pages/about.js
+++ pages/about.js
@@ -5,14 +5,14 @@ import { CardBio, Story } from "../components";
 export default function About() {
   const teams = [
     {
-      image: "/wahyu.jpg",
-      name: "Wahyu",
-      desc: "Seorang Front-End Developer. Saat ini fokus mengembangkan website di PukulEnam untuk berbagai klien.",
-    },
-    {
       image: "/hawa.png",
       name: "Haqila",
       desc: "Seorang mahasiswa Informatika di Telkom University. Saat ini fokus mempelajari teknologi web dan mobile.",
+    },
+    {
+      image: "/wahyu.jpg",
+      name: "Wahyu",
+      desc: "Seorang Front-End Developer. Saat ini fokus mengembangkan website di PukulEnam untuk berbagai klien.",
     },
   ];
 
@@ -28,7 +28,7 @@ export default function About() {
         <div className="flex flex-col items-center justify-center gap-y-2 text-brown-dark text-center mt-10">
           <h2 className="text-4xl font-Lora font-extrabold">Team</h2>
           <p>Descriptions</p>
-          <div className="flex flex-col lg:flex-row gap-6 rounded-lg px-8 lg:px-10 py-8 my-6">
+          <div className="flex flex-col lg:flex-row gap-6 rounded-lg px-6 lg:px-10 py-8 my-6">
             {teams.map((team) => (
               <CardBio key={team.name} team={team} />
             ))}

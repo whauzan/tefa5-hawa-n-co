a12996abd256497b84bb19eb25e18127a97b7cb4
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-10-17 15:47:44 +0700

    fix: security risk

--- components/Blog.js
+++ components/Blog.js
@@ -5,6 +5,7 @@ export default function Blog({ img, url, title, desc }) {
         href={url}
         className="transition block p-6 rounded-lg border border-brown-secondary shadow-md shadow-brown-primary hover:scale-105"
         target={"_blank"}
+        rel="noreferrer"
       >
         <img src={img} className="rounded-lg mb-4 object-cover h-48 w-96" />
         <h5 className="mb-2 text-2xl font-bold font-Lora tracking-tight text-brown-200 hover:text-white-700">
